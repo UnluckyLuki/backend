3084cceaa822c69e4e1e3ab3406a7a451b78da74
UnluckyLuki <themrluki43@gmail.com>
2025-05-30 09:26:24 +0200

    Added Phone model and PhoneDbContext.cs

---
+++ backend/Properties/Model/Phone.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace backend.Properties.Model;
+
+public class Phone
+{
+    
+    [Key,DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public int phoneId { get; set; }
+    [Required, MaxLength(9) , MinLength(9)]
+    public string phoneNumber { get; set; }
+    [Required, MaxLength(15)]
+    public string phoneType { get; set; }
+}
---
+++ backend/Properties/Model/PhoneDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace backend.Properties.Model;
+
+public class PhoneDbContext: DbContext
+{
+    public PhoneDbContext(DbContextOptions<PhoneDbContext> options) : base(options)
+    {
+        
+    }
+    
+    public DbSet<Phone> Phones { get; set; }
+}
